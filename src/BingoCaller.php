77c120cd1af76c377beb10db97b40a8a80f8add5
Daniel Soto <dasg.1993@gmail.com>
2021-11-18 15:20:37 -0500

    Bingo caller & 1st test error

--- src/BingoCaller.php
+++ src/BingoCaller.php
@@ -1,5 +1,12 @@
-<?php
+<?php namespace src;
 
 class BingoCaller {
+    private $numbers = [];
 
+    public  function __construct()
+    {
+    }
+    public function callNumber (){
+        return 0;
+    }
 }
---
+++ tests/BingoCallerTest.php
@@ -0,0 +1,23 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use src\BingoCaller;
+
+class BingoCallerTest extends TestCase {
+    public function testCallNumbersInValidRange(){
+        $caller = new BingoCaller;
+        $number = $caller->callNumber();
+        $this->assertTrue($number >= 1 && $number <=75);
+    }
+
+    public function testCall75Times(){
+        $caller = new BingoCaller();
+        $calledNumbers = [];
+        $expectedNumbers = range(1, 75);
+
+        for($i=1; $i<=75; ++$i){
+            $calledNumbers[] = $caller->callNumber()
+        }
+
+        $this->assertEquals($expectedNumbers, $calledNumbers);
+    }
+}
